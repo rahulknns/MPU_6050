50cfd1ecad0942f6a7eea57ae3456c9f06bbd24e
rahulknns <rahulknns@gmail.com>
2023-11-07 18:28:54 +0530

    fix sensitivity values

--- src/mpu6050.cpp
+++ src/mpu6050.cpp
@@ -7,7 +7,7 @@
  * @param preferred_wire - pointer to the preferred wire object
  * @param g - acceleration due to gravity
 */
-MPU6050::MPU6050(byte address = DEFAULT_ADDRESS, TwoWire* preferred_wire = &Wire,float g = ACCELERATION_DUE_TO_GRAVITY) : I2CDevice(address, preferred_wire)
+MPU6050::MPU6050(byte address = DEFAULT_ADDRESS, TwoWire* preferred_wire = &Wire,double g = ACCELERATION_DUE_TO_GRAVITY) : I2CDevice(address, preferred_wire)
 {
     _accelerometer_sensitivity = ACCELERO_METER_SENSITIVITY_2;
     _gyroscope_sensitivity = GYROSCOPE_SENSITIVITY_250;
@@ -120,13 +120,13 @@ void MPU6050::disableSleepMode()
  * @param az - acceleration in z direction
  * @note The acceleration values are in m/s^2
 */
-void MPU6050::getAccelerometerReadings(float& ax, float& ay, float& az)
+void MPU6050::getAccelerometerReadings(double& ax, double& ay, double& az)
 {
     short int raw_a[3];
     readShortIntsFromReg(ACCEL_XOUT_H_REG, 3,  raw_a);
-    ax = (float) _acceleration_due_to_gravity * raw_a[0] / _accelerometer_sensitivity;
-    ay = (float) _acceleration_due_to_gravity * raw_a[1] / _accelerometer_sensitivity;
-    az = (float) _acceleration_due_to_gravity * raw_a[2] / _accelerometer_sensitivity;
+    ax = (double) _acceleration_due_to_gravity * raw_a[0] * _accelerometer_sensitivity;
+    ay = (double) _acceleration_due_to_gravity * raw_a[1] * _accelerometer_sensitivity;
+    az = (double) _acceleration_due_to_gravity * raw_a[2] * _accelerometer_sensitivity;
 }
 
 /**
@@ -136,13 +136,13 @@ void MPU6050::getAccelerometerReadings(float& ax, float& ay, float& az)
  * @param gz - angular velocity in z direction
  * @note The angular velocity values are in rad/s
 */
-void MPU6050::getGyroscopeReadings(float& gx, float& gy, float& gz)
+void MPU6050::getGyroscopeReadings(double& gx, double& gy, double& gz)
 {
     short int raw_g[3];
     readShortIntsFromReg(GYRO_XOUT_H_REG, 3,  raw_g);
-    gx = (float) raw_g[0] / _gyroscope_sensitivity;
-    gy = (float) raw_g[1] / _gyroscope_sensitivity;
-    gz = (float) raw_g[2] / _gyroscope_sensitivity;
+    gx = (double) raw_g[0] * _gyroscope_sensitivity;
+    gy = (double) raw_g[1] * _gyroscope_sensitivity;
+    gz = (double) raw_g[2] * _gyroscope_sensitivity;
 }
 
 /**
@@ -155,7 +155,7 @@ void MPU6050::getGyroscopeReadings(float& gx, float& gy, float& gz)
  * @param gz - angular velocity in z direction
  * @note The acceleration values are in m/s^2 and the angular velocity values are in rad/s
 */
-void MPU6050::getSensorsReadings(float& ax, float& ay, float& az, float& gx, float& gy, float& gz)
+void MPU6050::getSensorsReadings(double& ax, double& ay, double& az, double& gx, double& gy, double& gz)
 {
     getAccelerometerReadings(ax, ay, az);
     getGyroscopeReadings(gx, gy, gz);
--- src/mpu6050.hpp
+++ src/mpu6050.hpp
@@ -5,23 +5,23 @@
 #include "I2C_driver/src/I2C_device.hpp"
 #include "mpu6050_registers.hpp"
 #define DEFAULT_ADDRESS 0x68 
-#define ACCELERO_METER_SENSITIVITY_2 16384.0
-#define ACCELERO_METER_SENSITIVITY_4 8192.0
-#define ACCELERO_METER_SENSITIVITY_8 4096.0
-#define ACCELERO_METER_SENSITIVITY_16 2048.0
-#define GYROSCOPE_SENSITIVITY_250 131.0
-#define GYROSCOPE_SENSITIVITY_500 65.5
-#define GYROSCOPE_SENSITIVITY_1000 32.8
-#define GYROSCOPE_SENSITIVITY_2000 16.4
-#define ACCELERATION_DUE_TO_GRAVITY 9.81
-#define ACCELERO_METER_RANGE_2 0
-#define ACCELERO_METER_RANGE_4 1
-#define ACCELERO_METER_RANGE_8 2
-#define ACCELERO_METER_RANGE_16 3
-#define GYROSCOPE_RANGE_250 0
-#define GYROSCOPE_RANGE_500 1
-#define GYROSCOPE_RANGE_1000 2
-#define GYROSCOPE_RANGE_2000 3
+#define ACCELERO_METER_SENSITIVITY_2 0.00003051757812
+#define ACCELERO_METER_SENSITIVITY_4 0.00006103515625
+#define ACCELERO_METER_SENSITIVITY_8 0.0001220703125
+#define ACCELERO_METER_SENSITIVITY_16 0.000244140625
+#define GYROSCOPE_SENSITIVITY_250 0.003814697266
+#define GYROSCOPE_SENSITIVITY_500 0.007629394531
+#define GYROSCOPE_SENSITIVITY_1000 0.01525878906
+#define GYROSCOPE_SENSITIVITY_2000 0.03051757812
+#define ACCELERATION_DUE_TO_GRAVITY 9.80665
+#define ACCELERO_METER_RANGE_2 0<<3
+#define ACCELERO_METER_RANGE_4 1<<3
+#define ACCELERO_METER_RANGE_8 2<<3
+#define ACCELERO_METER_RANGE_16 3<<3
+#define GYROSCOPE_RANGE_250 0<<3
+#define GYROSCOPE_RANGE_500 1<<3
+#define GYROSCOPE_RANGE_1000 2<<3
+#define GYROSCOPE_RANGE_2000 3<<3
 #define LOWPASS_CUTOFF_FREQUENCY_250 0
 #define LOWPASS_CUTOFF_FREQUENCY_184 1
 #define LOWPASS_CUTOFF_FREQUENCY_92 2
@@ -32,9 +32,9 @@
 class MPU6050 : public I2CDevice
 {
 private:
-    float _accelerometer_sensitivity, _gyroscope_sensitivity, _acceleration_due_to_gravity;
+    double _accelerometer_sensitivity, _gyroscope_sensitivity, _acceleration_due_to_gravity;
 public:
-    MPU6050(byte address = DEFAULT_ADDRESS, TwoWire* preferred_wire = &Wire,float g = ACCELERATION_DUE_TO_GRAVITY);
+    MPU6050(byte address = DEFAULT_ADDRESS, TwoWire* preferred_wire = &Wire,double g = ACCELERATION_DUE_TO_GRAVITY);
     ~MPU6050();
     setAccelerometerRange(byte range);
     setGyroscopeRange(byte range);
@@ -42,8 +42,8 @@ public:
     setLowpassCutOffFrequency(byte frequency);
     enableSleepMode();
     disableSleepMode();
-    getAccelerometerReadings(float& ax, float& ay, float& az);
-    getGyroscopeReadings(float& gx, float& gy, float& gz);
-    getSensorsReadings(float& ax, float& ay, float& az, float& gx, float& gy, float& gz);
+    getAccelerometerReadings(double& ax, double& ay, double& az);
+    getGyroscopeReadings(double& gx, double& gy, double& gz);
+    getSensorsReadings(double& ax, double& ay, double& az, double& gx, double& gy, double& gz);
 };
 #endif /* MPU6050_HPP_ */
